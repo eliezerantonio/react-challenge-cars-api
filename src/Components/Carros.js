cc0768e25d1838365650616f736315efd2f90a26
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-07 23:56:03 +0100

    carros

--- src/Components/Carros.js
+++ src/Components/Carros.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Carros = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [carros, setCarros] = React.useState(null);
 
-export default Carros
+  React.useEffect(() => {
+    fetch("https://carros-springboot.herokuapp.com/api/v1/carros")
+      .then((car) => car.json())
+      .then((json) => setCarros(json));
+  }, []);
+  if (carros === null) return null;
+    return <section>
+      
+        
+        
+  </section>;
+};
+
+export default Carros;
