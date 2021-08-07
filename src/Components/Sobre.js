50a86e55480c759592881b7ab322e396fcee3df2
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-07 22:56:41 +0100

    sobre componente

---
+++ src/Components/Sobre.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sobre = () => {
+    return (
+        <section clasName={`${styles.sonre} animeLeft`}>
+            
+        </section>
+    )
+}
+
+export default Sobre
